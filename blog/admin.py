d776aa1e7cf4a3b428732b51d3982b3d6a3b017d
cjй <sergeygurin2003f@gmail.com>
2024-06-15 21:43:52 +0300

    [1.2] Upgrade admin panel for model Post, autofield slug

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author'] #Правое боковое меню фильтрации
+    search_fields = ['title', 'body'] #Поля, по которым возможен поиск
+    prepopulated_fields = {'slug': ('title',)} #автоматически заполняет поле slug на основе значения поля title.
+    raw_id_fields = ['author'] #заменяет стандартное поле выбора (dropdown) для поля author на текстовое поле с возможностью ввода ID.
+    date_hierarchy = 'publish' #добавляет навигационную ссылку для фильтрации объектов по дате в верхней части административной панели.
+    ordering = ['status', 'publish'] #Сортировка, сначала по статусу, а потом по публикации
